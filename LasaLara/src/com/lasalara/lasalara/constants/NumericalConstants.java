fa915fd8ea56dd7ab625b32636eeea40cfe2e31f
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2014-10-07 19:07:46 +0300

    Back end: Added numerical constants class.

---
+++ LasaLara/src/com/lasalara/lasalara/constants/NumericalConstants.java
@@ -0,0 +1,10 @@
+package com.lasalara.lasalara.constants;
+
+/**
+ * Class for all of the numerical constants in the application.
+ * @author Ants-Oskar Mäesalu
+ */
+public class NumericalConstants {
+	// Database information
+	public static final int DATABASE_VERSION = 1;
+}
