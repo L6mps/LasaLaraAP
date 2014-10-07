8a578022aac4d58f5da508cd7ebe7c1b73132184
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2014-10-07 14:26:28 +0300

    Refactorised string constants. Resolves #63.

--- LasaLara/src/com/lasalara/lasalara/constants/StringConstants.java
+++ LasaLara/src/com/lasalara/lasalara/constants/StringConstants.java
@@ -5,9 +5,26 @@ package com.lasalara.lasalara.constants;
  * @author Ants-Oskar Mäesalu
  */
 public class StringConstants {
-	public static String APP_NAME = "LasaLara";
-	public static String URL_GET_BOOK = "http://www.lasalara.com/getbook";
-	public static String URL_GET_CHAPTERS = "http://www.lasalara.com/getmchapters";
-	public static String URL_GET_QUESTIONS = "http://www.lasalara.com/getquestions";
-	public static String URL_POSE_QUESTION = "http://www.lasalara.com/posequestion";
+	// General constants
+	public static final String APP_NAME = "LasaLara";
+	// Web request URL constants
+	public static final String URL_GET_BOOK = "http://www.lasalara.com/getbook";
+	public static final String URL_GET_CHAPTERS = "http://www.lasalara.com/getmchapters";
+	public static final String URL_GET_QUESTIONS = "http://www.lasalara.com/getquestions";
+	public static final String URL_POSE_QUESTION = "http://www.lasalara.com/posequestion";
+	// Database book table constants
+	public static final String BOOK_TABLE_NAME = "book";
+	public static final String BOOK_COLUMN_KEY = "key";
+	public static final String BOOK_COLUMN_TITLE = "title";
+	public static final String BOOK_COLUMN_OWNER_EMAIL = "ownerEmail";
+	public static final String BOOK_COLUMN_OWNER_NAME = "ownerName";
+	public static final String BOOK_COLUMN_OWNER_INSTITUTION = "ownerInstitution";
+	public static final String BOOK_COLUMN_LAST_CHAPTER = "lastChapter";
+	// Database chapter connection table constants
+	public static final String CHAPTER_CONNECTION_TABLE_NAME = "chapterInBook";
+	public static final String CHAPTER_CONNECTION_COLUMN_BOOK = "bookKey";
+	public static final String CHAPTER_CONNECTION_COLUMN_CHAPTER = "chapterKey";
+	// Database chapter table constants
+	public static final String CHAPTER_TABLE_NAME = "chapter";
+	public static final String CHAPTER_COLUMN_KEY = "key";
 }
--- LasaLara/src/com/lasalara/lasalara/database/BookOpenHelper.java
+++ LasaLara/src/com/lasalara/lasalara/database/BookOpenHelper.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.lasalara.lasalara.backend.Book;
 import com.lasalara.lasalara.backend.Chapter;
+import com.lasalara.lasalara.constants.StringConstants;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -16,34 +17,18 @@ public class BookOpenHelper extends SQLiteOpenHelper {
 	
 	public static final String DATABASE_NAME = "LasaLaraAP.db";;
 	private static final int DATABASE_VERSION = 1;
-
-	// TODO: Move to constants enumerator
-	private static final String TABLE_NAME = "book";
-	private static final String COLUMN_KEY = "key";
-	private static final String COLUMN_TITLE = "title";
-	private static final String COLUMN_OWNER_EMAIL = "ownerEmail";
-	private static final String COLUMN_OWNER_NAME = "ownerName";
-	private static final String COLUMN_OWNER_INSTITUTION = "ownerInstitution";
-	private static final String COLUMN_LAST_CHAPTER = "lastChapter";
-	
-	private static final String CHAPTERS_CONNECTION_TABLE_NAME = "chapterInBook";
-	private static final String CHAPTERS_CONNECTION_COLUMN_BOOK = "bookKey";
-	private static final String CHAPTERS_CONNECTION_COLUMN_CHAPTER = "chapterKey";
-	
-	private static final String CHAPTERS_TABLE_NAME = "chapters";
-	private static final String CHAPTERS_COLUMN_KEY = "key";
 	
 	private static final String TABLE_CREATE =
 			"CREATE TABLE " + 
-			TABLE_NAME + " (" +
-			COLUMN_KEY + " TEXT, " +
-			COLUMN_TITLE + " TEXT, " +
-			COLUMN_OWNER_EMAIL + " TEXT, " +
-			COLUMN_OWNER_NAME + " TEXT, " +
-			COLUMN_OWNER_INSTITUTION + " TEXT, " +
-			COLUMN_LAST_CHAPTER + " TEXT);";
+			StringConstants.BOOK_TABLE_NAME + " (" +
+			StringConstants.BOOK_COLUMN_KEY + " TEXT, " +
+			StringConstants.BOOK_COLUMN_TITLE + " TEXT, " +
+			StringConstants.BOOK_COLUMN_OWNER_EMAIL + " TEXT, " +
+			StringConstants.BOOK_COLUMN_OWNER_NAME + " TEXT, " +
+			StringConstants.BOOK_COLUMN_OWNER_INSTITUTION + " TEXT, " +
+			StringConstants.BOOK_COLUMN_LAST_CHAPTER + " TEXT);";
 	private static final String TABLE_DROP = 
-			"DROP TABLE IF EXISTS " + TABLE_NAME;
+			"DROP TABLE IF EXISTS " + StringConstants.BOOK_TABLE_NAME;
 
     BookOpenHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -66,20 +51,22 @@ public class BookOpenHelper extends SQLiteOpenHelper {
 	public boolean insertBook(Book book) {
 		SQLiteDatabase db = this.getReadableDatabase();
 		ContentValues contentValues = new ContentValues();
-		contentValues.put(COLUMN_KEY, book.getKey());
-		contentValues.put(COLUMN_TITLE, book.getTitle());
-		contentValues.put(COLUMN_OWNER_EMAIL, book.getOwnerEmail());
-		contentValues.put(COLUMN_OWNER_NAME, book.getOwnerName());
-		contentValues.put(COLUMN_OWNER_INSTITUTION, book.getOwnerInstitution());
-		contentValues.put(COLUMN_LAST_CHAPTER, book.getLastChapter());
-		db.insert(TABLE_NAME, null, contentValues);
+		contentValues.put(StringConstants.BOOK_COLUMN_KEY, book.getKey());
+		contentValues.put(StringConstants.BOOK_COLUMN_TITLE, book.getTitle());
+		contentValues.put(StringConstants.BOOK_COLUMN_OWNER_EMAIL, book.getOwnerEmail());
+		contentValues.put(StringConstants.BOOK_COLUMN_OWNER_NAME, book.getOwnerName());
+		contentValues.put(StringConstants.BOOK_COLUMN_OWNER_INSTITUTION, book.getOwnerInstitution());
+		contentValues.put(StringConstants.BOOK_COLUMN_LAST_CHAPTER, book.getLastChapter());
+		db.insert(StringConstants.BOOK_TABLE_NAME, null, contentValues);
 		return true; // TODO: Why do we need this?
 	}
 	
 	public List<Book> getBooks(String bookKey) {
 		List<Book> bookList = new ArrayList<Book>();
 		SQLiteDatabase db = this.getReadableDatabase();
-		Cursor results =  db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_KEY + "=" + bookKey, null);
+		String selectBooksQuery = "SELECT * FROM " + StringConstants.BOOK_TABLE_NAME + 
+				" WHERE " + StringConstants.BOOK_COLUMN_KEY + "=" + bookKey;
+		Cursor results =  db.rawQuery(selectBooksQuery, null);
 		results.moveToFirst();
 		while (!results.isAfterLast()) {
 			bookList.add(new Book(results));
@@ -91,7 +78,16 @@ public class BookOpenHelper extends SQLiteOpenHelper {
 	public List<Chapter> getChapters(String bookKey) {
 		List<Chapter> chapterList = new ArrayList<Chapter>();
 		SQLiteDatabase db = this.getReadableDatabase();
-		Cursor results =  db.rawQuery("SELECT * FROM " + CHAPTERS_TABLE_NAME + " INNER JOIN " + CHAPTERS_CONNECTION_TABLE_NAME + " ON " + CHAPTERS_CONNECTION_TABLE_NAME + "." + CHAPTERS_CONNECTION_COLUMN_CHAPTER + "=" + CHAPTERS_TABLE_NAME + "." + CHAPTERS_COLUMN_KEY + " WHERE " + CHAPTERS_CONNECTION_TABLE_NAME + "." + CHAPTERS_CONNECTION_COLUMN_BOOK + "=" + COLUMN_KEY, null);
+		String selectChaptersQuery =
+				"SELECT * FROM " + StringConstants.CHAPTER_TABLE_NAME + 
+				" INNER JOIN " + StringConstants.CHAPTER_CONNECTION_TABLE_NAME + 
+				" ON " + StringConstants.CHAPTER_CONNECTION_TABLE_NAME + 
+				"." + StringConstants.CHAPTER_CONNECTION_COLUMN_CHAPTER + 
+				"=" + StringConstants.CHAPTER_TABLE_NAME + "." + StringConstants.CHAPTER_COLUMN_KEY + 
+				" WHERE " + StringConstants.CHAPTER_CONNECTION_TABLE_NAME + 
+				"." + StringConstants.CHAPTER_CONNECTION_COLUMN_BOOK + 
+				"=" + bookKey;
+		Cursor results =  db.rawQuery(selectChaptersQuery, null);
 		results.moveToFirst();
 		while (!results.isAfterLast()) {
 			// TODO: chapterList.add(new Chapter(results));
