3b4be8008e5f1d14e005aac5c69ac2f08666656b
Ants-Oskar Mäesalu <aomaesalu@gmail.com>
2014-12-04 10:35:20 +0200

    Back end: Started implementing the backup service. #57

---
+++ LasaLara/src/com/lasalara/lasalara/Backup.java
@@ -0,0 +1,26 @@
+package com.lasalara.lasalara;
+
+import java.io.IOException;
+
+import android.app.backup.BackupAgent;
+import android.app.backup.BackupDataInput;
+import android.app.backup.BackupDataOutput;
+import android.os.ParcelFileDescriptor;
+
+public class Backup extends BackupAgent {
+
+	@Override
+	public void onBackup(ParcelFileDescriptor arg0, BackupDataOutput arg1,
+			ParcelFileDescriptor arg2) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onRestore(BackupDataInput arg0, int arg1,
+			ParcelFileDescriptor arg2) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
